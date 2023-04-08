88520191a9c421dd105cc9a8bbe3621fe632f8a0
lichunxiao <28961716@qq.com>
2023-04-08 20:15:43 +0800

    init

---
+++ 网页/美军二战步兵营营部编制 - 体制编制 - 知远防务论坛 ----学术性_资料性_研讨性！ - Powered by Discuz!_files/online_v3.php
@@ -0,0 +1,6 @@
+var node=document.getElementById('cnzz_stat_icon_'+2596302);
+if(node){
+    node.innerHTML="<a href=https://www.cnzz.com/stat/website.php?web_id=2596302 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=2596302 target=_blank> |  昨日IP[97] | 昨日PV[681]</a>";
+}else{
+    document.write("<a href=https://www.cnzz.com/stat/website.php?web_id=2596302 target=_blank>站长统计</a><a href=https://www.cnzz.com/stat/website.php?web_id=2596302 target=_blank> |  昨日IP[97] | 昨日PV[681]</a>");
+}
